37e2ce848fd2711d73007fd77b7b82ee599df2f4
Hugo Fernando <huginho888@outlook.com>
2021-10-25 23:37:58 -0300

    Adicionando header

---
+++ script.js
@@ -0,0 +1,42 @@
+//Express
+  const express = require('express');
+  const app = express();
+  const port = 8081;
+  const hostname = '127.0.0.1';
+
+
+
+  //Handlebars
+    const handlebars = require('express-handlebars');
+    app.engine('handlebars', handlebars({defaultLayout:'main'}));
+    app.set('view engine', 'handlebars');
+
+
+
+  //bodyParser
+    const bodyParser = require('body-parser');
+    app.use(bodyParser.urlencoded({extended:false}));
+    app.use(bodyParser.json());
+
+
+  
+  //Rotas do express
+   
+    app.get('/', (req,res)=>{
+        res.render('home')
+    })
+    
+    app.get('/estilo', (req,res)=>{
+        res.sendFile(__dirname + '/css/style.css')
+    })
+
+    app.get('/search', (req,res)=>{
+        res.sendFile(__dirname + '/imagens/search.jpg')
+    })
+
+
+
+  //Ligando Servidor
+    app.listen(port,hostname,()=>{
+        console.log('Servidor iniciado')
+    })
