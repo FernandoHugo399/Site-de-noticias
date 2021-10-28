a25f35161c200dfc8837aeaec628badd9b8ae500
Hugo Fernando <huginho888@outlook.com>
2021-10-28 13:48:34 -0300

    pesquisar v3

--- public/js/script.js
+++ public/js/script.js
@@ -15,7 +15,16 @@
     const header = document.querySelector("header");
     addEventListener('resize', ()=>{
         if(document.body.clientWidth > 1035){
-            menu.classList.remove('mobaw')
+            menu.classList.remove('mobaw');
+            logo.style.display = 'block';
+            fechar.style.display = 'none';
+            barra.style.display = 'none';
+            search.style.display = 'none';
+        } else if (document.body.clientWidth < 1035){
+            search.style.display = 'block';
+            logo.style.display = 'block';
+            fechar.style.display = 'none';
+            barra.style.display = 'none';
         }
     })
 
@@ -27,3 +36,35 @@
         }
     })
 
+
+
+
+//search
+  const barra = document.querySelectorAll('.barrapesquisa')[0];
+  const search = document.querySelectorAll('.search img')[0];
+  const logo = document.querySelectorAll('.logo')[0];
+  const fechar = document.querySelectorAll('.close')[0];
+  search.addEventListener('click', ()=>{
+    if(logo.classList.contains('none')){
+
+        logo.style.display = 'block';
+        search.style.display = 'block';
+        barra.style.display = 'none';
+
+    } else {
+        logo.style.display = 'none';
+        search.style.display = 'none';
+        barra.style.display = 'block';
+        fechar.style.display = 'block';
+    }
+
+  })
+
+  fechar.addEventListener('click', ()=>{
+      if(fechar.style.display == 'block'){
+          search.style.display = 'block';
+          fechar.style.display = 'none';
+          barra.style.display = 'none';
+          logo.style.display = 'block';
+      } else{}
+  })
